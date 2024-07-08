6551f2586a80b60d7b2f30567b78abb4fffc7928
Federico Lopez <federicolopez99@gmail.com>
2024-07-08 19:00:15 -0300

    cambio nombre database
     de usurios_db a contactos_db.

--- backend/db/db.js
+++ backend/db/db.js
@@ -2,7 +2,7 @@ const mySql = require('mysql2');
 const connection = mySql.createConnection(
     {
         //host: 'localhost'||'172.20.141.9',
-        host: '172.20.141.9',
+        host: 'localhost',
         user: 'root',
         password: 'root',
         database: 'usuarios_db',
@@ -22,7 +22,7 @@ const connection = mySql.createConnection(
         console.log("Conectado a la base de datos");
 
 
-        connection.query('CREATE DATABASE IF NOT EXISTS usuarios_db', (err, results) => {
+        connection.query('CREATE DATABASE IF NOT EXISTS contactos_db', (err, results) => {
         if (err) {
             console.log("Error creando la base de datos");
             return;
@@ -32,22 +32,15 @@ const connection = mySql.createConnection(
         console.log("Base de datos asegurada");
 
 
-        connection.changeUser({ database: 'usuarios_db' }, (err) => {
+        connection.changeUser({ database: 'contactos_db' }, (err) => {
             if (err) {
-                console.error("Error al cambiar a usuarios_db", err);
+                console.error("Error al cambiar a contactos_db", err);
                 return;
             }
 
 
 // La tabla usuarios_db, borrarla cuando no se use mas.
             const createTableQuery = `                
-                CREATE TABLE IF NOT EXISTS usuarios_db ( 
-                    id INT AUTO_INCREMENT PRIMARY KEY,
-                    nombre VARCHAR(100) NOT NULL,
-                    apellido VARCHAR(100) NOT NULL,
-                    mail VARCHAR(255) NOT NULL
-                ); 
-
                 CREATE TABLE IF NOT EXISTS usuarios (
                     id_usuario INT AUTO_INCREMENT PRIMARY KEY,
                     nombre VARCHAR(255) NOT NULL,
